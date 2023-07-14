811c2b15bd36f7f213ecd44634c3f551842f5bf5
naveenkumarm18177 <137770593+naveenkumarm18177@users.noreply.github.com>
2023-07-14 14:20:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function change(){
+    var img=document.getElementById("bulb");
+    if(img.src.match("off"))
+    {
+        img.src="on.jpg" ; 
+
+    }
+    else{
+        img.src="off.jpg";
+    }
+}
